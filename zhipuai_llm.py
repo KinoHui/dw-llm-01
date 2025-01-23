6c5b610e8fbde7e89f3938392a38d4adb6684898
j.7 <119877898+KinoHui@users.noreply.github.com>
2025-01-23 03:07:53 +0000

    Update .gitignore and remove ignored files from tracking

---
+++ zhipuai_llm.py
@@ -0,0 +1,69 @@
+import os
+
+from dotenv import load_dotenv, find_dotenv
+
+# 读取本地/项目的环境变量。
+
+# find_dotenv() 寻找并定位 .env 文件的路径
+# load_dotenv() 读取该 .env 文件，并将其中的环境变量加载到当前的运行环境中  
+# 如果你设置的是全局的环境变量，这行代码则没有任何作用。
+_ = load_dotenv(find_dotenv())
+
+from zhipuai import ZhipuAI
+
+client = ZhipuAI(
+    api_key=os.environ["ZHIPUAI_API_KEY"]
+)
+
+def gen_glm_params(prompt):
+    '''
+    构造 GLM 模型请求参数 messages
+
+    请求参数：
+        prompt: 对应的用户提示词
+    '''
+    messages = [{"role": "user", "content": prompt}]
+    return messages
+
+
+def get_completion(prompt, model="glm-4", temperature=0.95):
+    '''
+    获取 GLM 模型调用结果
+
+    请求参数：
+        prompt: 对应的提示词
+        model: 调用的模型，默认为 glm-4，也可以按需选择 glm-3-turbo 等其他模型
+        temperature: 模型输出的温度系数，控制输出的随机程度，取值范围是 0~1.0，且不能设置为 0。温度系数越低，输出内容越一致。
+    '''
+
+    messages = gen_glm_params(prompt)
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        temperature=temperature
+    )
+    if len(response.choices) > 0:
+        return response.choices[0].message.content
+    return "generate answer error"
+
+prompt = f"""
+请生成包括书名、作者和类别的三本虚构的、非真实存在的中文书籍清单，\
+并以 JSON 格式提供，其中包含以下键:book_id、title、author、genre。
+"""
+# print(get_completion(prompt)) 
+
+def zhipu_embedding(text: str):
+    
+    response = client.embeddings.create(
+        model="embedding-2",
+        input=text,
+    )
+    return response
+
+text = '要生成 embedding 的输入文本，字符串形式。'
+response = zhipu_embedding(text=text)
+print(f'response类型为：{type(response)}')
+print(f'embedding类型为：{response.object}')
+print(f'生成embedding的model为：{response.model}')
+print(f'生成的embedding长度为：{len(response.data[0].embedding)}')
+print(f'embedding（前10）为: {response.data[0].embedding[:10]}')
